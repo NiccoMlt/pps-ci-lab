b36fb68d2ed2e29ac11f335d1ab6c7b1a00226da
luca.semprini10 <luca.semprini10@studio.unibo.it>
2018-04-26 10:20:57 +0200

    Aggiunta riga

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PRAISE THE LEADER");
+        System.out.println("THE LEADER BROKE MY HEART");
+    }
+}
